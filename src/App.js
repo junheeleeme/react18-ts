fecc39e5b27399171d4d6980edd86f6f81e13c4c
JunHee <jhlee@infracube.co.kr>
2022-07-10 21:51:06 +0900

    utils/DocTitle 분리

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import { useEffect } from "react";
-import { Routes, Route, useParams, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import DocTitle from "./Utils/Doctitle";
 import { pages } from "./pages";
 import Header from "./Layout/Header";
 import Main from "./Layout/Main";
 import Footer from "./Layout/Footer";
 
 export default function App() {
-  const location = useLocation();
-  const params = useParams();
-
-  const title = process.env.TITLE;
-  console.log(location, params);
-  console.log(title);
-
-  //   useEffect(() => {
-  //     const { title: subTitle } = pages.find((p) => p.path === pathname);
-  //     if (subTitle === "Home") document.title = title;
-  //     else document.title = title + " - " + subTitle;
-  //   }, [pathname]);
-
   return (
     <>
+      <DocTitle />
       <Header />
       <Main>
         <Routes>
