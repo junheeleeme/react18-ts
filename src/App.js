9206bbdc1eba5ac9d949940caea64b6a0215effd
JunHee <jhlee@infracube.co.kr>
2022-07-10 21:29:43 +0900

    webpack.definePlugin -> dotenv-webpack으로 변경

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import { useEffect } from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom' 
-import { pages } from './pages'
-import Header from "./Layout/Header"
-import Main from "./Layout/Main"
-import Footer from "./Layout/Footer"
-
+import { useEffect } from "react";
+import { Routes, Route, useParams, useLocation } from "react-router-dom";
+import { pages } from "./pages";
+import Header from "./Layout/Header";
+import Main from "./Layout/Main";
+import Footer from "./Layout/Footer";
 
 export default function App() {
+  const location = useLocation();
+  const params = useParams();
 
-    const { pathname } = useLocation()
-    const title = process.env.TITLE
+  const title = process.env.TITLE;
+  console.log(location, params);
+  console.log(title);
 
-    useEffect(()=> {
-        const { title: subTitle } = pages.find(p => p.path === pathname )
-        if(subTitle === 'Home') document.title = title
-        else document.title = title + ' - ' + subTitle
-    }, [pathname])
+  //   useEffect(() => {
+  //     const { title: subTitle } = pages.find((p) => p.path === pathname);
+  //     if (subTitle === "Home") document.title = title;
+  //     else document.title = title + " - " + subTitle;
+  //   }, [pathname]);
 
-    return(
-    <> 
-        <Header />
-        <Main>
-            <Routes>
-                { pages.map(p => <Route key={p.id} path={p.path} element={p.comp} /> ) }
-            </Routes>
-        </Main>
-        <Footer/>
+  return (
+    <>
+      <Header />
+      <Main>
+        <Routes>
+          {pages.map((p) => (
+            <Route key={p.id} path={p.path} title={p.title} element={p.comp} />
+          ))}
+        </Routes>
+      </Main>
+      <Footer />
     </>
-    )
-
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,78 @@
-const webpack = require('webpack')
-const path = require('path')
-const dotenv = require("dotenv")
-dotenv.config()
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-    name: 'React18-webpack-babel-setting',
-    mode: 'development', // production, development
-    devtool: 'eval',
+const isDevelopment = process.env.NODE_ENV !== "production";
+const envPath = `./.env.${isDevelopment ? "development" : "production"}`;
+
+module.exports = () => {
+  dotenv.config({
+    path: envPath,
+  });
+
+  return {
+    name: "React18-webpack-setting",
+    mode: isDevelopment ? "development" : "production", // production, development
+    devtool: !isDevelopment ? "hidden-source-map" : "eval",
     entry: {
-        app: './src/index.js',
+      app: "./src/index.js",
     },
     module: {
-        rules: [
-            {   // 리액트 바벨 설정
-                test: /\.js/, 
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
+      rules: [
+        {
+          // 리액트 바벨 설정
+          test: /\.js/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
             },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "image/[contenthash].[ext]",
             },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: 'image/[contenthash].[ext]'
-                    }
-                } 
-                
-            }
-        ]
+          },
+        },
+      ],
     },
     plugins: [
-        new CleanWebpackPlugin(),
-        new webpack.DefinePlugin({
-            "process.env": JSON.stringify(process.env)
-        }),
-        new HtmlWebpackPlugin(
-            { 
-                template: './public/index.html', // 템플릿 설정
-                title: process.env.TITLE, // 문서 타이틀
-                minify: true, // 압축 설정
-            }
-        ),
-        new webpack.ProvidePlugin({
-            "React": "react",
-        }),
+      new Dotenv({
+        path: envPath,
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html", // 템플릿 설정
+        title: process.env.TITLE, // 문서 타이틀
+        minify: false, // 압축 설정
+      }),
+      new CleanWebpackPlugin(),
+      new webpack.ProvidePlugin({
+        React: "react",
+      }),
     ],
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
+      path: path.resolve(__dirname, "dist"),
+      filename: "app.js",
     },
-    devServer: { // 개발 서버 설정
-        port: 3000,
-        hot: true, // 핫 모듈 교체(HMR) 활성화
-        compress: true,
-        open: true,
-        historyApiFallback: true,
-    }
-}
+    devServer: {
+      // 개발 서버 설정
+      port: 3000,
+      hot: true, // 핫 모듈 교체(HMR) 활성화
+      compress: true,
+      open: true,
+      historyApiFallback: true,
+    },
+  };
+};
